53930dbebbddd7505ebcbb512362c2d291824298
samigos25 <samigos25@users.noreply.github.com>
2019-03-06 16:34:49 +0300

    work

--- Lesson-5/Task2/Message.cs
+++ Lesson-5/Task2/Message.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Task2
 {
